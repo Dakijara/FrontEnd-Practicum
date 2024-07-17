f5acb7dd871be3737c352a2c5444b3d6d4e0e3cf
Danila <danila.kosyak@gmail.com>
2024-07-17 20:44:49 +0300

    Kucha_Izmeneniy

---
+++ hk_boss_helper/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Main from "./pages/main.jsx"
+
+const App = () => {
+  return (
+    <div>
+        <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ hk_boss_helper/src/pages/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import classes from "./main.css"
+import logo from "../assets/LOGO2.png"
+import cinzel from "../assets/fonts/Cinzel-VariableFont_wght.ttf"
+const Main = () => {
+    return(
+        
+        <body>
+                <div class="panel">
+                        <a href={Main}>
+                            <img src={logo} height={150}/>
+                        </a>
+                    <a href="" class="panelURL">Список Боссов</a>
+                    <a href="" class="panelURL">Тир-листы боссов</a>
+                    <a href="" class="panelURL">Категории</a>
+                    <a href="" class="panelURL">Карта боссов</a>
+                    </div>
+                <div class="bot">
+                    
+                    <div class="textbox">
+                        Привет! Рад приветствовать на помощнике по тренировке боссов в Hollow Knight, этот сайт предназначен как для новичков, у которых возникли трудности с прохождением того или иного босса на любом этапе игры, так и для игроков, стремящихся пройти более поздний контент, как например, пятый пантеон. <br></br>
+                        Здесь собранна информация о каждом боссе, об удобных сборках и оптимальных стратегиях борьбы с ними
+                    </div>
+                </div>
+        </body>
+    )
+};
+export default Main;
